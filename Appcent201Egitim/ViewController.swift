12fa726fe9afa88256a6044fac610fcaccbdb300
FurKAnRA <furkankara1402@gmail.com>
2021-11-20 20:23:10 +0300

    Initial Commit

---
+++ Appcent201Egitim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Appcent201Egitim
+//
+//  Created by Furkan Kara on 20.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
